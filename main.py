3bb4c6125eaa86e94f9abfc05f8bff4e2dfda55a
Chloe-sev <136395526+Chloe-sev@users.noreply.github.com>
2023-06-13 00:06:52 +0000

    Made webpages dynamic

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask! test'
+    return render_template('index.html')
 
+@app.route('/webpages')
+def render_webpage():
+  return
+  render_template('webpages.html')
+
+@app.route('/styles')
+def render_styles():
+  return
+  render_styles('styles.html')
 
 app.run(host='0.0.0.0', port=81)
